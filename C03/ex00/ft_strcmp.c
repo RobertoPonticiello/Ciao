ff1b72381ee59848c3f5d2bb4d40c9c561e098a8
Roberto nicola Ponticiello <rpontici@e3r8p1.42roma.it>
2024-11-18 19:21:38 +0100

    ciao

---
+++ C03/ex00/ft_strcmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpontici <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 14:28:32 by rpontici          #+#    #+#             */
+/*   Updated: 2024/11/18 18:49:52 by rpontici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	if (s1[i] == s2[i])
+	{
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+int	main(void)
+{
+	char	s1[] = "cane";
+	char	s2[] = "ciao";
+
+	printf("%d", strcmp(s1, s2));
+	printf("%d", ft_strcmp(s1, s2));
+	return (0);
+}
